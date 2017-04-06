4a91286b1fd236f71b4ee3443b3310b2a7ed45c8
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2017-04-06 03:25:55 +0300

    replace assigned pin with a numbered pin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Map, TileLayer } from "react-leaflet";
+import L from "leaflet";
 import MarkerClusterGroup from "./third-party/MarkerClusterGroup";
 import { bounds, createIcon } from "./gen-points";
 import "./App.css";
@@ -13,7 +14,10 @@ export const mapState = state => ({
 });
 
 export const mapActions = dispatch => ({
-  pointerOver: ({ _icon, options: { id, size, name, type, address } }) =>
+  pointerOver: (
+    { _icon, options: { id, sorted, size, name, type, address } }
+  ) =>
+    sorted ||
     dispatch({
       type: "holder->show",
       rect: _icon.getBoundingClientRect(),
@@ -34,106 +38,146 @@ export const mapActions = dispatch => ({
 export const prevent = event => event.preventDefault();
 
 export class App extends Component {
-  render() {
+  renderMap() {
+    const {
+      points,
+      orders,
+      pointerOver
+    } = this.props;
+
+    return (
+      <div className="map-holder">
+        <Map bounds={bounds} maxZoom={18}>
+          <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+          <MarkerClusterGroup
+            onMarkerMouseOver={pointerOver}
+            markers={(points || []).concat(
+              orders.map((order, index) => ({
+                ...order,
+                options: {
+                  ...orders.options,
+                  sorted: true,
+                  icon: new L.DivIcon({
+                    className: "work-order-sorted-icon",
+                    html: index + 1
+                  })
+                }
+              }))
+            )}
+          />
+        </Map>
+      </div>
+    );
+  }
+
+  renderHolder() {
+    const {
+      pointerOut,
+      holder
+    } = this.props;
+
+    return holder &&
+      <div
+        className={`draggable-holder draggable-holder--${holder.data.type}`}
+        draggable="true"
+        onDragStart={event => {
+          event.dataTransfer.setData("text/plain", JSON.stringify(holder.data));
+        }}
+        onDragEnd={pointerOut}
+        style={{
+          top: holder.top - 18,
+          left: holder.left - 18
+        }}
+      >
+        <div className="draggable-holder--hide" onClick={pointerOut} />
+        <div className="draggable-holder--name">{holder.data.name}</div>
+        <div className="draggable-holder--size">
+          {holder.data.size} YARDS
+        </div>
+        <div className="draggable-holder--address">
+          {holder.data.address}
+        </div>
+      </div>;
+  }
+
+  renderList() {
     const {
       moveToList,
       startMoveOutList,
       stopMoveOutList,
-      moveOutList,
-      pointerOver,
-      pointerOut,
-      points,
-      orders,
-      holder,
-      dropArea
+      orders
     } = this.props;
 
     return (
-      <div>
-        <div className="map-holder">
-          <Map bounds={bounds} maxZoom={18}>
-            <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-            <MarkerClusterGroup
-              onMarkerMouseOver={pointerOver}
-              markers={points}
-            />
-          </Map>
-        </div>
-        {holder &&
+      <div
+        className="list-holder"
+        onDragEnter={prevent}
+        onDragOver={prevent}
+        onDrop={event => {
+          const data = JSON.parse(event.dataTransfer.getData("text"));
+          if (!data.id) {
+            return;
+          }
+          moveToList(data);
+        }}
+      >
+        {orders.map(order => (
           <div
-            className={`draggable-holder draggable-holder--${holder.data.type}`}
+            key={order.options.id}
+            className={
+              `list-holder--item list-holder--item--${order.options.type}`
+            }
             draggable="true"
             onDragStart={event => {
-              event.dataTransfer.setData(
-                "text/plain",
-                JSON.stringify(holder.data)
-              );
+              event.dataTransfer.setData("text/plain", JSON.stringify(order));
+              startMoveOutList(order);
             }}
-            onDragEnd={pointerOut}
-            style={{
-              top: holder.top - 18,
-              left: holder.left - 18
-            }}
+            onDragEnd={stopMoveOutList}
           >
-            <div className="draggable-holder--hide" onClick={pointerOut} />
-            <div className="draggable-holder--name">{holder.data.name}</div>
-            <div className="draggable-holder--size">
-              {holder.data.size} YARDS
+            <div className="list-holder--item--name">
+              {order.options.name}
             </div>
-            <div className="draggable-holder--address">
-              {holder.data.address}
+            <div className="list-holder--item--size">
+              {order.options.size} YARDS
             </div>
-          </div>}
-        <div
-          className="list-holder"
-          onDragEnter={prevent}
-          onDragOver={prevent}
-          onDrop={event => {
-            const data = JSON.parse(event.dataTransfer.getData("text"));
-            if (!data.id) {
-              return;
-            }
-            moveToList(data);
-          }}
-        >
-          {orders.map(order => (
-            <div
-              key={order.options.id}
-              className={
-                `list-holder--item list-holder--item--${order.options.type}`
-              }
-              draggable="true"
-              onDragStart={event => {
-                event.dataTransfer.setData("text/plain", JSON.stringify(order));
-                startMoveOutList(order);
-              }}
-              onDragEnd={stopMoveOutList}
-            >
-              <div className="list-holder--item--name">
-                {order.options.name}
-              </div>
-              <div className="list-holder--item--size">
-                {order.options.size} YARDS
-              </div>
-              <div className="list-holder--item--address">
-                {order.options.address}
-              </div>
+            <div className="list-holder--item--address">
+              {order.options.address}
             </div>
-          ))}
-        </div>
-        {dropArea &&
-          <div
-            className="drop-area"
-            onDragEnter={prevent}
-            onDragOver={prevent}
-            onDrop={event => {
-              const data = JSON.parse(event.dataTransfer.getData("text"));
-              const { options = {} } = data;
-              options.icon = createIcon(options);
-              moveOutList(data);
-              stopMoveOutList();
-            }}
-          />}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  renderDropArea() {
+    const {
+      stopMoveOutList,
+      moveOutList,
+      dropArea
+    } = this.props;
+
+    return dropArea &&
+      <div
+        className="drop-area"
+        onDragEnter={prevent}
+        onDragOver={prevent}
+        onDrop={event => {
+          const data = JSON.parse(event.dataTransfer.getData("text"));
+          const { options = {} } = data;
+          options.icon = createIcon(options);
+          moveOutList(data);
+          stopMoveOutList();
+        }}
+      />;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderMap()}
+        {this.renderHolder()}
+        {this.renderList()}
+        {this.renderDropArea()}
       </div>
     );
   }
